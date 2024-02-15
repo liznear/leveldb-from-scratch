57823b8703d4bee8975c49ef398178b254860e56
Liz <liz@lizmore.dev>
2024-02-15 13:29:49 +0800

    Add encoding and decoding for kv

--- model/kv.go
+++ model/kv.go
@@ -1,5 +1,21 @@
 package model
 
+import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
+)
+
+type Key struct {
+	Data string
+}
+
+func NewKey(s string) *Key {
+	return &Key{
+		Data: s,
+	}
+}
+
 type Value struct {
 	Data []byte
 }
@@ -8,4 +24,68 @@ func NewValue(value []byte) Value {
 	return Value{
 		Data: value,
 	}
+}
+
+type KV struct {
+	Key   Key
+	Value Value
+}
+
+func (kv *KV) ToBytes() []byte {
+	ret := bytes.Buffer{}
+	_ = binary.Write(&ret, binary.BigEndian, uint32(len(kv.Key.Data)))
+	ret.WriteString(kv.Key.Data)
+	_ = binary.Write(&ret, binary.BigEndian, uint32(len(kv.Value.Data)))
+	_, _ = ret.Write(kv.Value.Data)
+	return ret.Bytes()
+}
+
+func BatchFromBytes(bytes []byte) ([]KV, int, error) {
+	var ret []KV
+	i := 0
+	for i < len(bytes) {
+		kv, nextI, err := FromBytes(bytes[i:])
+		if err != nil {
+			return ret, i, err
+		}
+		i += nextI
+		ret = append(ret, *kv)
+	}
+	return ret, i, nil
+}
+
+func FromBytes(bytes []byte) (*KV, int, error) {
+	checkLength := func(b []byte, l int) error {
+		if len(b) < l {
+			return fmt.Errorf("kv: length is too short: %v", b)
+		}
+		return nil
+	}
+
+	if err := checkLength(bytes, 4); err != nil {
+		return nil, 0, err
+	}
+	kLen := int(binary.BigEndian.Uint32(bytes))
+	if err := checkLength(bytes, 4+kLen); err != nil {
+		return nil, 0, err
+	}
+	key := bytes[4 : 4+kLen]
+
+	if err := checkLength(bytes, 8+kLen); err != nil {
+		return nil, 0, err
+	}
+	vLen := int(binary.BigEndian.Uint32(bytes[4+kLen:]))
+
+	if err := checkLength(bytes, 8+kLen+vLen); err != nil {
+		return nil, 0, err
+	}
+	value := bytes[4+kLen+4 : 4+kLen+4+vLen]
+	return &KV{
+		Key: Key{
+			Data: string(key),
+		},
+		Value: Value{
+			Data: value,
+		},
+	}, 8 + kLen + vLen, nil
 }
---
+++ model/kv_test.go
@@ -0,0 +1,97 @@
+package model
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestKV(t *testing.T) {
+	tcs := []struct {
+		name  string
+		key   string
+		value []byte
+	}{
+		{
+			name:  "NilValue",
+			key:   "",
+			value: nil,
+		},
+		{
+			name:  "Empty",
+			key:   "",
+			value: []byte{},
+		},
+		{
+			name:  "EmptyKey",
+			key:   "",
+			value: []byte("World"),
+		},
+		{
+			name:  "EmptyValue",
+			key:   "Hello",
+			value: []byte{},
+		},
+		{
+			name:  "KeyValue",
+			key:   "Hello",
+			value: []byte("World"),
+		},
+	}
+
+	for _, tc := range tcs {
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+			kv := KV{
+				Key:   *NewKey(tc.key),
+				Value: *NewValue(tc.value),
+			}
+			bytes := kv.ToBytes()
+			got, end, err := FromBytes(bytes)
+			if err != nil {
+				t.Fatalf("Fail to convert kv (%q:%v) to bytes: %v", tc.key, tc.value, err)
+			}
+			if got.Key.Data != tc.key {
+				t.Errorf("Got key %q, want %q", got.Key.Data, tc.key)
+			}
+			if !reflect.DeepEqual(got.Value.Data, tc.value) {
+				t.Errorf("Got value %v, want %v", got.Value.Data, tc.value)
+			}
+			if end != len(bytes) {
+				t.Errorf("Still has %d bytes not processed", len(bytes)-end)
+			}
+		})
+	}
+}
+
+func TestKV_BatchFromBytes(t *testing.T) {
+	t.Parallel()
+	kvs := []KV{
+		{
+			Key:   *NewKey("Key1"),
+			Value: *NewValue([]byte("Value1")),
+		},
+		{
+			Key:   *NewKey("Key2"),
+			Value: *NewValue([]byte("Value2")),
+		},
+	}
+	var bs []byte
+	for _, kv := range kvs {
+		bs = append(bs, kv.ToBytes()...)
+	}
+	got, end, err := BatchFromBytes(bs)
+	if err != nil {
+		t.Fatalf("Fail to convert %v to multiple kvs: %v", bs, err)
+	}
+	if len(got) != len(kvs) {
+		t.Errorf("Got %d kvs, want %d", len(got), len(kvs))
+	}
+	for i := range got {
+		if !reflect.DeepEqual(got[i], kvs[i]) {
+			t.Errorf("%d: Got %v, want %v", i, got[i], kvs[i])
+		}
+	}
+	if end != len(bs) {
+		t.Errorf("Still has %d bytes not processed", len(bs)-end)
+	}
+}
